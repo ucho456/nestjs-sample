204facb681307ec210c41d6c684265c1f1160392
ucho456 <watapqxyz@gmail.com>
2022-10-12 20:45:37 +0900

    Create item table

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,7 @@ import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    ItemsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      autoLoadEntities: true,
-    }),
-  ],
+  imports: [ItemsModule, TypeOrmModule.forRoot()],
   controllers: [],
   providers: [],
 })
