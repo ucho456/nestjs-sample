2ae53542a9cdd6aded0522c724770ec793eb4980
ucho456 <watapqxyz@gmail.com>
2022-10-14 19:47:09 +0900

    Hash the password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,11 +1,14 @@
 import { User } from 'src/entities/user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const user = this.create({ ...createUserDto });
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(createUserDto.password, salt);
+    const user = this.create({ ...createUserDto, password: hashPassword });
     await this.save(user);
     return user;
   }
