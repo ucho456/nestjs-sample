a83789014d38eb632690087e875967809348949c
ucho456 <watapqxyz@gmail.com>
2022-10-14 18:57:17 +0900

    Create [POST /auth/signup]

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async signup(createUserDto: CreateUserDto): Promise<User> {
+    return await this.userRepository.createUser(createUserDto);
+  }
+}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,12 @@
+import { User } from 'src/entities/user.entity';
+import { EntityRepository, Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.create({ ...createUserDto });
+    await this.save(user);
+    return user;
+  }
+}
